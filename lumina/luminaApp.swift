2762001e309c6603eb26728f10642d7a4ed37f65
Martin THOMAS <mts13hotmail.fr>
2026-05-30 12:59:13 +0200

    first commit

---
+++ lumina/luminaApp.swift
@@ -0,0 +1,17 @@
+//
+//  luminaApp.swift
+//  lumina
+//
+//  Created by Martin Thomas on 29/05/2026.
+//
+
+import SwiftUI
+
+@main
+struct luminaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
